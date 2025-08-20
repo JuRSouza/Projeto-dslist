2c04954674872e2a8457faea28341b8a4066030e
Juliana Souza <121642110+JuRSouza@users.noreply.github.com>
2025-08-20 18:57:39 -0300

    Game, repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+	
+	private Long id;
+	private String title; // título
+	private Integer year; //ano -> year é uma palavra reservada no BD
+	private String imgUrl; // url da imagem
+	private String shortDescription; //descrição curta
+	
+	
+	//Construtor vazio
+	public GameMinDTO() {
+	}
+
+
+	
+	//Construtor que recebe a entidade
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public Integer getYear() {
+		return year;
+	}
+
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
